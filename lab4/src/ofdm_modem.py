f8a8fc388e2edc74cc7a50b3068901954c3dda41
isvoropaev02 <is.voropaev@gmail.com>
2025-10-15 21:00:04 +0300

    ofdm refactored

--- lab4/src/ofdm_modem.py
+++ lab4/src/ofdm_modem.py
@@ -3,6 +3,7 @@ from src.dpsk_modem import DPSKModem
 
 class OFDMModem:
     def __init__(self, num_sc: int = 1) -> None:
+        assert num_sc % 2 == 0, "num_sc should be even"
         self.__num_sc = num_sc
         # self.__cp_len = int(num_sc * 4.69 / 71.35) # https://www.sharetechnote.com/html/5G/5G_FrameStructure.html
         self.__cp_len = max(int(num_sc * 0.2), 1)
@@ -18,21 +19,20 @@ class OFDMModem:
 
     def ofdm_modulate(self, samples: np.ndarray, fs_hz: int=-1, scs_hz: int = 15) -> np.ndarray:
         if fs_hz == -1:
-            fft_samples = samples
+            nfft = self.__num_sc
             cp_len = self.__cp_len
         else:
             nfft = int(fs_hz/scs_hz)
             cp_len = int(nfft * 0.2)
-            fft_samples = np.zeros(nfft, dtype=np.complex128)
-            fft_samples[:self.__num_sc//2] = samples[:self.__num_sc//2]
-            fft_samples[-self.__num_sc//2:] = samples[-self.__num_sc//2:]
+        fft_samples = np.zeros(nfft, dtype=np.complex128)
+        fft_samples[:self.__num_sc//2] = samples[-self.__num_sc//2:]
+        fft_samples[-self.__num_sc//2:] = samples[:self.__num_sc//2]
         td_signal = np.fft.ifft(fft_samples, norm='ortho')
         return np.concat([td_signal[len(td_signal)-cp_len:], td_signal])
     
     def ofdm_demodulate(self, signal: np.ndarray, fs_hz: int=-1, scs_hz: int = 15) -> np.ndarray:
         # trimed_signal = signal[(self.__cp_len // 2) : len(signal) + (-self.__cp_len // 2)]
         if fs_hz == -1:
-            fft_samples = signal
             cp_len = self.__cp_len
         else:
             nfft = int(fs_hz/scs_hz)
@@ -40,8 +40,8 @@ class OFDMModem:
         trimed_signal = signal[cp_len:]
         fd_signal = np.fft.fft(trimed_signal, norm='ortho')
         fft_samples = np.zeros(self.__num_sc, dtype=np.complex128)
-        fft_samples[:self.__num_sc//2] = fd_signal[:self.__num_sc//2]
-        fft_samples[-self.__num_sc//2:] = fd_signal[-self.__num_sc//2:]
+        fft_samples[:self.__num_sc//2] = fd_signal[-self.__num_sc//2:]
+        fft_samples[-self.__num_sc//2:] = fd_signal[:self.__num_sc//2]
         return fft_samples
     
     def process_tx(self, in_bits: np.ndarray, modem_block: DPSKModem, num_symb: int, fs_hz: int=-1, scs_hz: int = 15) -> np.ndarray:
